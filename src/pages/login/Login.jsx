cb102834090c8dbedeec08067e30650401ffe2fa
Bysern <ernestbystrzycki@gmail.com>
2021-12-21 17:26:39 +0100

    implementing login functionality using authContext

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,21 @@
 import "./login.css"
+import { useContext, useRef } from "react";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@mui/material";
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+    const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        loginCall({ email: email.current.value, password: password.current.value }, dispatch);
+    };
+    console.log(user);
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -10,13 +25,13 @@ export default function Login() {
 
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input type="email" className="loginInput" placeholder="Email" />
-                        <input type="password" className="loginInput" placeholder="Password" />
-                        <button className="loginButton">Login</button>
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input type="email" className="loginInput" placeholder="Email" ref={email} required />
+                        <input type="password" className="loginInput" placeholder="Password" ref={password} required minLength="5" />
+                        <button className="loginButton">{isFetching ? <CircularProgress color="info" /> : "Login"}</button>
                         <span className="loginForgot">Forgot Password?</span>
-                        <button className="loginRegisterButton">Create a New Account</button>
-                    </div>
+                        <button className="loginRegisterButton">{isFetching ? <CircularProgress color="info" /> : "Create New Account"}</button>
+                    </form>
                 </div>
 
             </div>
